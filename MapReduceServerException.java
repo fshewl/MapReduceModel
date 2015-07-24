b6f207053d0f5442ae8bcfff305e5f48d4ad2173
Wenlei He <fshewl@gmail.com>
2015-07-24 11:32:26 -0700

    MapReduce Server Exception class

---
+++ MapReduceServerException.java
@@ -0,0 +1,27 @@
+import java.lang.Exception;
+import java.lang.Throwable;
+
+public class MapReduceServerException extends Exception {
+    
+    public MapReduceServerException() {
+	
+    }
+
+    public MapReduceServerException(String name) {
+	super(name);
+    }
+
+    public MapReduceServerException(Throwable throwable) {
+	super(throwable);
+    }
+
+    public MapReduceServerException(String name, Throwable throwable) {
+	super(name, throwable);
+    }
+
+
+    public static void main(String[] argv) throws MapReduceServerException {
+	throw new MapReduceServerException("MapReduce Server Exception.");
+
+    }
+}
